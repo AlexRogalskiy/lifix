accc0a27812e22866ae271d3f9631a066c7f45ea
ch4mpy <ch4mp@c4-soft.com>
2021-07-26 18:53:32 -1000

    atttempt with @AotProxyHint

--- api/faults-endpoints/src/main/java/com/c4_soft/lifix/FaultsEndpointsApplication.java
+++ api/faults-endpoints/src/main/java/com/c4_soft/lifix/FaultsEndpointsApplication.java
@@ -2,11 +2,14 @@ package com.c4_soft.lifix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.nativex.hint.AotProxyHint;
+import org.springframework.nativex.hint.ProxyBits;
 
 import com.c4_soft.lifix.common.storage.FileSystemStorageService;
 import com.c4soft.commons.security.WebSecurityConfig;
 import com.c4soft.commons.web.CommonExceptionHandlers;
 
+@AotProxyHint(targetClass = com.c4_soft.lifix.web.FaultController.class, proxyFeatures = ProxyBits.IS_STATIC)
 @SpringBootApplication(scanBasePackageClasses = {
         FaultsEndpointsApplication.class,
         FileSystemStorageService.class,
